b8e98f3758c88f9ce6fd434029dc52aa4ec27e6b
Emily Charles <echarles@bostonproductions.com>
2020-05-06 00:06:11 -0400

    Minor front end work on connect page

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import axios from "axios";
+import classnames from "classnames";
 
 import { CSSTransition } from "react-transition-group";
 
@@ -16,14 +17,18 @@ import { Topic } from "../ui/topic";
 interface IConnectState {
   mode: "LEARN" | "TEACH";
   userData?: IUserData;
-  topics?: string[];
+  selectedToLearn: string[];
+  selectedToTeach: string[];
+  allTopics?: string[];
 }
 
 export class Connect extends React.Component<{}, IConnectState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      mode: "LEARN"
+      mode: "LEARN",
+      selectedToLearn: [],
+      selectedToTeach: []
     };
 
     // retrieve user
@@ -43,7 +48,9 @@ export class Connect extends React.Component<{}, IConnectState> {
             email: res.data.data.email,
             learn: res.data.data.learn,
             teach: res.data.data.teach
-          }
+          },
+          selectedToLearn: res.data.data.learn ? res.data.data.learn : [],
+          selectedToTeach: res.data.data.teach ? res.data.data.teach : []
         });
       })
       .catch(err => {
@@ -59,7 +66,7 @@ export class Connect extends React.Component<{}, IConnectState> {
           topics = topics.concat(topicObj.name);
         });
         this.setState({
-          topics: topics
+          allTopics: topics
         });
       })
       .catch(err => {
@@ -72,7 +79,7 @@ export class Connect extends React.Component<{}, IConnectState> {
     if (dataExists(window.localStorage["authToken"])) {
       return (
         <div id="connect">
-          {!this.state.userData && !this.state.topics ? (
+          {!this.state.userData && !this.state.allTopics ? (
             <div>"Loading..."</div>
           ) : (
             <div />
@@ -111,15 +118,20 @@ export class Connect extends React.Component<{}, IConnectState> {
           </div>
           <div id="topic-bank">
             <CSSTransition
-              in={dataExists(this.state.topics)}
+              in={dataExists(this.state.allTopics)}
               classNames="fade"
               timeout={500}
               unmountOnExit={true}
             >
               <div>
-                {this.state.topics && this.renderTopics(this.state.topics)}
+                {this.state.allTopics &&
+                  this.renderTopics(this.state.allTopics)}
               </div>
             </CSSTransition>
+          </div>
+
+          <div>
+            Look for {this.state.mode === "LEARN" ? "teachers" : "students"}
           </div>
         </div>
       );
@@ -133,15 +145,69 @@ export class Connect extends React.Component<{}, IConnectState> {
     }
   }
 
+  private handleTopicSelection = (topic: string) => {
+    // add this topic if it wasn't already there; remove it if it was
+    this.setState(prevState => {
+      if (prevState.mode === "TEACH") {
+        const topicIndex = prevState.selectedToTeach.indexOf(topic);
+        if (topicIndex === -1) {
+          prevState.selectedToTeach.push(topic);
+        } else {
+          prevState.selectedToTeach.splice(topicIndex, 1);
+        }
+        return {
+          selectedToLearn: prevState.selectedToLearn,
+          selectedToTeach: prevState.selectedToTeach
+        };
+      } else {
+        const topicIndex = prevState.selectedToLearn.indexOf(topic);
+        if (topicIndex === -1) {
+          prevState.selectedToLearn.push(topic);
+        } else {
+          prevState.selectedToLearn.splice(topicIndex, 1);
+        }
+        return {
+          selectedToLearn: prevState.selectedToLearn,
+          selectedToTeach: prevState.selectedToTeach
+        };
+      }
+    });
+  };
+
   private renderTopics = (topics: string[] | undefined) => {
+    if (!this.state.userData) {
+      return <div>Please sign in again</div>;
+    }
     if (topics === undefined || topics.length === 0) {
       return <div>Looks like you don't have any topics yet. Add some?</div>;
     }
 
+    let userTopics: string[] | undefined =
+      this.state.mode === "TEACH"
+        ? this.state.userData!.teach
+        : this.state.userData!.learn;
+
+    if (userTopics === undefined) {
+      userTopics = [];
+    }
+
     const topicContents = topics.map((topic, index) => {
+      const topicClasses = classnames({
+        "bank-item": true,
+        "selected-teach":
+          this.state.mode === "TEACH" &&
+          this.state.selectedToTeach.indexOf(topic) !== -1,
+        "selected-learn":
+          this.state.mode === "LEARN" &&
+          this.state.selectedToLearn.indexOf(topic) !== -1
+      });
       return (
-        <div key={"topic-" + index}>
-          <Topic name={topic} editable={true} />
+        <div key={"topic-" + index} className={topicClasses}>
+          <Topic
+            name={topic}
+            editable={true}
+            onClick={this.handleTopicSelection}
+          />
         </div>
       );
     });
--- src/ui/topic.tsx
+++ src/ui/topic.tsx
@@ -5,6 +5,7 @@ import "../css/topic.css";
 interface ITopicProps {
   name: string;
   editable: boolean;
+  onClick?: (topicName: string) => void;
 }
 export class Topic extends React.Component<ITopicProps, {}> {
   constructor(props: ITopicProps) {
@@ -13,9 +14,18 @@ export class Topic extends React.Component<ITopicProps, {}> {
 
   public render() {
     return (
-      <div className="topic rounded center-contents">
+      <div
+        className="topic rounded center-contents"
+        onPointerUp={this.handleClick}
+      >
         <div>{this.props.name}</div>
       </div>
     );
   }
+
+  private handleClick = (e: React.PointerEvent) => {
+    if (this.props.onClick) {
+      this.props.onClick(this.props.name);
+    }
+  };
 }
